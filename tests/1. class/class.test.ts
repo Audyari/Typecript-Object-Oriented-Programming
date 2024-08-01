81840ab79afb5aba8b1a249061b2856916892334
Audyari Wiyono <audy123ari@gmail.com>
2024-08-01 14:35:27 +0700

    1. class

---
+++ tests/1. class/class.test.ts
@@ -0,0 +1,21 @@
+
+// Membuat Class
+
+describe('Membuat Class', () => {
+
+    it('Membuat Class', () => {
+
+        // Membuat Class
+        class Customer {
+        
+        }
+
+        class Order {
+        
+        }
+
+        const customer: Customer = new Customer();
+        const order: Order = new Order();
+
+    })
+})
